6ba1623c2541911ef989561eeab33ad6e6c7c008
Leonardo Castro <leonardoaii71@gmail.com>
2019-09-17 17:43:30 -0400

    new unity project

---
+++ Pong/Pong/Assets/Scripts/PlayerController.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+    bool isPlayerOne;
+    float movementSpeed = 1f;
+    const float lowerLimit = -3.4f, upperLimit = 3.4f;
+    Vector3 deltaPos;
+    // Start is called before the first frame update
+    void Start()
+    {
+        isPlayerOne = name == "player1";
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        deltaPos = new Vector3();
+       
+    }
+}
