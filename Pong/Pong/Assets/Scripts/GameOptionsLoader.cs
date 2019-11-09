130c2f30a2d821bb66218e9601fdb82d3f214f5f
Leonardo <leonardoaii71@gmail.com>
2019-11-09 00:23:49 -0400

    minor fixes

---
+++ Pong/Pong/Assets/Scripts/GameOptionsLoader.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using System.IO;
+using System.Runtime.Serialization;
+public class GameOptionsLoader : MonoBehaviour
+{
+
+    private void Start() {
+        LoadGameOptions();
+        Debug.Log(PlayerPrefs.GetString("PlayerName", ""));
+    }
+
+   string localPath = "gameOptions.xml";
+    public void SaveGameOptions(){
+       using(Stream fileStream = new FileStream(Application.persistentDataPath + "\\" + 
+       localPath, FileMode.Create))
+       {
+           DataContractSerializer dataContract = new DataContractSerializer(typeof(GameOptions));
+           dataContract.WriteObject(fileStream, GameOptions.Instance);
+       }
+
+    }
+
+     public void LoadGameOptions(){
+       using(Stream fileStream = new FileStream(Application.persistentDataPath + "\\" + 
+       localPath, FileMode.Open))
+       {
+           DataContractSerializer dataContract = new DataContractSerializer(typeof(GameOptions));
+           dataContract.ReadObject(fileStream);
+       }
+
+    }
+
+
+    
+}
